d0d1e2985a835048a68fd40f4001a773d86d6080
TerryFogg <Terry.Fogg@outlook.com>
2026-09-05 21:02:35 +1000

    wip

--- CMake/binutils.ESP32.cmake
+++ CMake/binutils.ESP32.cmake
@@ -5,106 +5,46 @@
 
 include(binutils.common)
 
-
-# find a set of files on a list of possible locations
 macro(nf_find_esp32_files_at_location files locations)
-
     foreach(SRC_FILE ${files})
-
         set(IDF_SRC_FILE SRC_FILE -NOTFOUND)
-
         find_file(IDF_SRC_FILE ${SRC_FILE}
             PATHS 
-
             ${locations}
-
             CMAKE_FIND_ROOT_PATH_BOTH
         )
-
-        if (BUILD_VERBOSE)
-            message("${SRC_FILE} >> ${IDF_SRC_FILE}")
-        endif()
-
         list(APPEND ESP32_IDF_SOURCES ${IDF_SRC_FILE})
-
     endforeach()
-
 endmacro()
 
 function(nf_set_optimization_options target) 
-
-    # debug compile options: -Og (optimize for debugging) and -g (produce generic debug symbols)
     target_compile_options(${target} PRIVATE
         $<$<CONFIG:Debug>:-Og -g>
         $<$<CONFIG:Release>:-O3>
         $<$<CONFIG:MinSizeRel>:-Os>
         $<$<CONFIG:RelWithDebInfo>:-Os -g>
     )
-
 endfunction()
-
-
 function(nf_set_linker_file target linker_file_name)
-
-
-    # # set linker file name
-    # set_target_properties(${TARGET} PROPERTIES LINK_FLAGS "-T${LINKER_FILE_NAME}")
-
 endfunction()
-
-
-
-# setting compile definitions for a target based on general build options
-# TARGET parameter to set the target that's setting them for
-# optional EXTRA_COMPILE_DEFINITIONS with compiler definitions to be added to the library
-# optional BUILD_TARGET when target it's a library pass here the name ot the target that's building for (either nanoBooter or nanoCLR)
 macro(nf_set_compile_definitions)
-
-    # parse arguments
     cmake_parse_arguments(NFSCD "" "TARGET" "EXTRA_COMPILE_DEFINITIONS;BUILD_TARGET" ${ARGN})
-
     if(NOT NFSCD_TARGET OR "${NFSCD_TARGET}" STREQUAL "")
         message(FATAL_ERROR "Need to set TARGET argument when calling nf_set_compile_definitions()")
     endif()
-
-    # definition for platform 
     nf_common_compiler_definitions(TARGET ${NFSCD_TARGET} BUILD_TARGET ${NFSCD_BUILD_TARGET})
-
-    # include extra compiler definitions
     target_compile_definitions(${NFSCD_TARGET} PUBLIC ${NFSCD_EXTRA_COMPILE_DEFINITIONS})
-
 endmacro()
-
-
-# Add packages that are common to ESP32 platform builds
-# To be called from target CMakeList.txt
-# optional TARGET argument with target name
 macro(nf_add_platform_packages)
-
-    # parse arguments
     cmake_parse_arguments(NFAPP "" "TARGET" "" ${ARGN})
-
     find_package(ESP32_IDF REQUIRED QUIET)
-
-    # packages specific for nanoCLR
     if("${NFAPP_TARGET}" STREQUAL "${NANOCLR_PROJECT_NAME}")
-
         if(USE_NETWORKING_OPTION)
-
             find_package(NF_Network REQUIRED QUIET)
-
         endif()
-
     endif()
-    
 endmacro()
-
-# Add ESP32  platform dependencies to a specific CMake target
-# To be called from target CMakeList.txt
 macro(nf_add_platform_dependencies target)
-
-
-
     configure_file(${BASE_PATH_FOR_CLASS_LIBRARIES_MODULES}/target_platform.h.in
                    ${CMAKE_BINARY_DIR}/targets/ESP32/ESP32_P4/target_platform.h @ONLY)
 
@@ -214,37 +154,19 @@ endmacro()
 # Add ESP32 platform target sources to a specific CMake target
 # To be called from target CMakeList.txt
 macro(nf_add_platform_sources target)
-
-    # add header files with common OS definitions and board definitions
     configure_file(${CMAKE_CURRENT_SOURCE_DIR}/target_common.h.in
                    ${CMAKE_CURRENT_BINARY_DIR}/target_common.h @ONLY)
-
-    # sources common to both builds
-    target_sources(${target}.elf PUBLIC
-    
-        ${TARGET_ESP32_IDF_COMMON_SOURCES}
-
-    )
-
-    # sources specific to nanoCLR
-    if(${target} STREQUAL ${NANOCLR_PROJECT_NAME})
-           
-        # add header with target platform definitions
-        configure_file(${CMAKE_SOURCE_DIR}/CMake/ESP32_target_os.h.in
+    configure_file(${CMAKE_SOURCE_DIR}/CMake/ESP32_target_os.h.in
                        ${CMAKE_BINARY_DIR}/targets/ESP32/target_os.h @ONLY)
-
-        configure_file(${CMAKE_CURRENT_SOURCE_DIR}/nanoCLR/target_board.h.in
+    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/nanoCLR/target_board.h.in
                        ${CMAKE_CURRENT_BINARY_DIR}/nanoCLR/target_board.h @ONLY)
 
-        target_sources(${target}.elf PUBLIC
-
-            ${TARGET_ESP32_IDF_NANOCLR_SOURCES}
-            ${ESP32_IDF_SOURCES}
-        )
-
-    endif()
-
-    if(USE_SECURITY_MBEDTLS_OPTION)
+    target_sources(nanoCLR.elf PUBLIC
+                   ${TARGET_ESP32_IDF_COMMON_SOURCES}
+                   ${TARGET_ESP32_IDF_NANOCLR_SOURCES}
+                   ${ESP32_IDF_SOURCES}
+    )
+ if(USE_SECURITY_MBEDTLS_OPTION)
         target_link_libraries(${target}.elf
         mbedtls
         )
@@ -253,90 +175,47 @@ macro(nf_add_platform_sources target)
     endif()
 
 endmacro()
-
-# macro to setup the build for a target
-# mandatory CLR_LINKER_FILE with the path to the linker file for nanoCLR
-# optional CLR_EXTRA_SOURCE_FILES with paths to extra files to be added to the nanoCLR build target
-# optional CLR_EXTRA_COMPILE_DEFINITIONS extra nanoCLR compile definitions to pass to nf_set_compile_definitions() 
-# optional CLR_EXTRA_LINKMAP_PROPERTIES extra nanoCLR link map properties to pass to nf_set_link_map() 
-# optional CLR_EXTRA_LINK_FLAGS extra nanoCLR link flags to pass to nf_set_link_options() 
 macro(nf_setup_target_build)
-
-    # parse arguments
     cmake_parse_arguments(
         NFSTBC 
         "" 
         "CLR_LINKER_FILE;CLR_EXTRA_LINKMAP_PROPERTIES;CLR_EXTRA_COMPILE_DEFINITIONS;CLR_EXTRA_COMPILE_OPTIONS;CLR_EXTRA_LINK_FLAGS" 
         "CLR_EXTRA_SOURCE_FILES;CLR_EXTRA_LIBRARIES" 
         ${ARGN})
-
-    #######################################
-    # now the actual calls for building a target
-
-    # add packages
     nf_add_common_packages()
     nf_add_platform_packages()
-
-
-    #######################
-    # nanoCLR executable has already been created on main CMakeLists
-    # just add any extra source files here
-    target_sources(
-        ${NANOCLR_PROJECT_NAME}.elf PUBLIC
-
-        # extra build files 
+    target_sources( nanoCLR.elf PUBLIC
         ${NFSTBC_CLR_EXTRA_SOURCE_FILES}
     )
 
     nf_add_platform_packages(TARGET ${NANOCLR_PROJECT_NAME})
     nf_add_platform_dependencies(${NANOCLR_PROJECT_NAME})
-    
-    # can't call  nf_add_common_sources() just like with the other platforms
-    # so need to replicate the code here  
-    target_sources(${NANOCLR_PROJECT_NAME}.elf PUBLIC
-    
-        ${CMAKE_CURRENT_SOURCE_DIR}/target_common.c
-        ${CMAKE_CURRENT_SOURCE_DIR}/target_BlockStorage.c
-        ${CMAKE_SOURCE_DIR}/src/PAL/BlockStorage/nanoPAL_BlockStorage.c
-        ${COMMON_PROJECT_SOURCES}
-        ${NF_HALCore_SOURCES}
+    target_sources(nanoCLR.elf PUBLIC
+                   ${CMAKE_CURRENT_SOURCE_DIR}/target_common.c
+                   ${CMAKE_CURRENT_SOURCE_DIR}/target_BlockStorage.c
+                   ${CMAKE_SOURCE_DIR}/src/PAL/BlockStorage/nanoPAL_BlockStorage.c
+                   ${COMMON_PROJECT_SOURCES}
+                   ${NF_HALCore_SOURCES}
+    )
+    target_link_libraries(nanoCLR.elf
+                          NF_CoreCLR
+                          NF_NativeAssemblies
+                          NF_Debugger
+                          WireProtocol
+                          ${NFACS_EXTRA_LIBRARIES}
     )
 
-    target_link_libraries(${NANOCLR_PROJECT_NAME}.elf
-        NF_CoreCLR
-        NF_NativeAssemblies
-        NF_Debugger
-        WireProtocol
-        
-        ${NFACS_EXTRA_LIBRARIES}
-    )
-
-    target_sources(${NANOCLR_PROJECT_NAME}.elf PUBLIC
-
+    target_sources(nanoCLR.elf PUBLIC
         ${NANOCLR_PROJECT_SOURCES}
-
-        # sources for nanoFramework libraries
         ${Graphics_Sources}
     )
-
     nf_add_platform_sources(${NANOCLR_PROJECT_NAME})
-
-    # include directories for nanoCLR
     nf_add_common_include_directories(${NANOCLR_PROJECT_NAME})
     nf_add_platform_include_directories(${NANOCLR_PROJECT_NAME})
-
-    # set compile options
     nf_set_compile_options(TARGET ${NANOCLR_PROJECT_NAME}.elf EXTRA_COMPILE_OPTIONS ${NFSTBC_BOOTER_EXTRA_COMPILE_OPTIONS})
-
-    # set compile definitions
     nf_set_compile_definitions(TARGET ${NANOCLR_PROJECT_NAME}.elf EXTRA_COMPILE_DEFINITIONS ${NFSTBC_CLR_EXTRA_COMPILE_DEFINITIONS} BUILD_TARGET ${NANOCLR_PROJECT_NAME} )
-
-    # set link options
     nf_set_link_options(TARGET ${NANOCLR_PROJECT_NAME}.elf EXTRA_LINK_FLAGS ${NFSTBC_CLR_EXTRA_LINK_FLAGS})
-
-    # this where all the **magic** happens with the IDF build
     idf_build_executable(${NANOCLR_PROJECT_NAME}.elf)
-
 endmacro()
 
 # macro that setups the calls to the partition tool to generate the various partitions
@@ -792,28 +671,4 @@ macro(nf_add_idf_as_library)
         COMMAND ${output_idf_size}
         --archives ${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.map
         COMMENT "Output IDF size summary")
-
-endmacro()
-
-# macro to clear binary files related with nanoBooter from output
-# to make sure that the build file it's up to date
-macro(nf_clear_output_files_nanobooter)
-    message("nanoCLR for ESP32 doesn't have a nanoBooter! Check the build options.")
-endmacro()
-
-# macro to clear binary files related with nanoCLR from output
-# to make sure that the build file it's up to date
-macro(nf_clear_output_files_nanoclr)
-
-    list(APPEND CLR_BUILD_FILES_TO_REMOVE ${CMAKE_BINARY_DIR}/${NANOCLR_PROJECT_NAME}.bin)
-    list(APPEND CLR_BUILD_FILES_TO_REMOVE ${CMAKE_BINARY_DIR}/${NANOCLR_PROJECT_NAME}.elf)
-
-    add_custom_command(
-        TARGET ${NANOCLR_PROJECT_NAME}.elf
-        PRE_BUILD
-        COMMAND ${CMAKE_COMMAND} -E remove -f ${CLR_BUILD_FILES_TO_REMOVE}
-        COMMAND_EXPAND_LISTS
-        COMMENT "Removing nanoCLR bin and elf files from build folder"
-    )
-
 endmacro()
---
+++ targets/ESP32/ESP32_P4/configuration/target_platform.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#define ESP32_RESERVE_SPIRAM_IDF_ALLOCATION_BYTES 262144
+#define ESP32_RESERVE_IRAM_IDF_ALLOCATION_KB      0
+
+#define NANOCLR_GRAPHICS   TRUE
+#define HAL_USE_SPI        FALSE
+#define HAL_USE_UART       FALSE
+#define HAL_USE_SDC        FALSE
+#define HAL_USE_BLE        FALSE
+#define SDC_MAX_OPEN_FILES 5
